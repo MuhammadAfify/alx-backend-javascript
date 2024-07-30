cecc5f9d3bd424ae3e085dc5a7fdd80bc9e9f625
MuhammadAfify <muhammadafefy@gmail.com>
2024-07-30 07:56:36 +0000

    Array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,8 @@
-let getListStudents = [
-    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-    {id: 2, firstName: 'James', location: Columbia},
-    {id: 5, firstName: 'Serena', location: 'San Francisco'}
-]
+function getListStudents(){
+    let stdData =  [
+        {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+        {id: 2, firstName: 'James', location: 'Columbia'},
+        {id: 5, firstName: 'Serena', location: 'San Francisco'}
+    ];
+    return stdData;
+}
