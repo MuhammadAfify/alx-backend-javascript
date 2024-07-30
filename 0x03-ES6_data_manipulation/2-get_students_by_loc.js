872219dafd194e8e9202a9b57063c409996466b6
MuhammadAfify <muhammadafefy@gmail.com>
2024-07-30 15:33:06 +0000

    function returns an array of objects who are located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 function getStudentsByLocation(stdListLocation, city) {
     return stdList.filter((stdListLocation) => stdListLocation.location === city)
 }
-export default getListStudentIds;
+export default getStudentsByLocation;
