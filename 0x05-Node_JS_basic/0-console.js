23529643b8220d8fa735ad5cda810854f86d4935
MuhammadAfify <muhammadafefy@gmail.com>
2024-11-29 21:54:27 +0000

    NodeJS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,4 @@
-/**
- * displayMessage - prints in STDOUT the string argument.
- * message: The message to display.
- */
 const displayMessage = (message) => {
 	console.log(message);
-  };
+};
 module.exports = displayMessage;
